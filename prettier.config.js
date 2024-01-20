62fab05ad43c0a583fde8a247564f7f6efc2eb9e
p-renner <phillianus@gmail.com>
2024-01-20 18:07:03 +0100

    feat(tutorial): add chapters 1-5

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,8 @@
 const styleguide = require('@vercel/style-guide/prettier');
 
 module.exports = {
-  ...styleguide,
-  plugins: [...styleguide.plugins, 'prettier-plugin-tailwindcss'],
+    ...styleguide,
+    plugins: [...styleguide.plugins, 'prettier-plugin-tailwindcss'],
+    tabWidth: 4,
+    useTabs: true,
 };
